f22b89badd710ede0bcb8e8eefe16d4c8049feca
61114440694 <sakkarin.ha.61@ubu.ac.th>
2021-03-15 02:49:55 +0700

    completeRoute

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import Home from './page/home/home.js'
 import Register from './page/register/register.js'
 import Login from './page/login/login.js'
 import Profile from './page/profile/profile.js'
-import {BrowserRouter,Router,Route} from 'react-router-dom'
+import {BrowserRouter,Route, Router} from 'react-router-dom'
+import history from './history/history';
 function App() {
     return (
         <BrowserRouter>
-                    <Route  exact={true} path="/" component={Home}></Route>
-                    <Route  exact={true} path="/Home" component={Home}></Route>
+        <Router history={history}>
+                    <Route exact={true} path="/" component={Home}></Route>
+                    <Route exact={true} path="/Home" component={Home}></Route>
                     <Route  path="/Login" component={Login}></Route>
                     <Route  path="/Profile" component={Profile} ></Route>
-                    <Route  path="/Register" component={Register} ></Route>
+                    <Route  path="/Register" component={Register} ></Route>  
+        </Router>
+                             
         </BrowserRouter>
     )
 }
