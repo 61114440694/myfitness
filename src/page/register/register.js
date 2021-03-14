d81f93a78347b31c055ca2d5f200ad6f43c5fa1f
Universz666 <kittiphon.pa.61@ubu.ac.th>
2021-03-15 03:28:30 +0700

    80%%%%%%%%%%%

--- src/page/register/register.js
+++ src/page/register/register.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-
+import React, { useRef, useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+// import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { useAuth } from "../contexts/AuthContext";
+import { Alert } from "react-bootstrap";
 
 const useStyles = makeStyles((theme) => ({
-  
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -39,20 +38,45 @@ const useStyles = makeStyles((theme) => ({
 export default function Login() {
   const classes = useStyles();
 
-  return (
-  
+  const fNameRef = useRef();
+  const lNameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-      
-    <Container  component="main" maxWidth="xs">
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match");
+    }
+
+      try {
+        setError("");
+        setLoading(true);
+        signup(emailRef.current.value, passwordRef.current.value);
+      } catch {
+        setError("Failed to create an account");
+    };
+
+    setLoading(false);
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-        Sign-up
+          Sign-up
         </Typography>
-        <form className={classes.form} noValidate>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -63,6 +87,7 @@ export default function Login() {
                 fullWidth
                 id="firstName"
                 label="First Name"
+                ref={fNameRef}
                 autoFocus
               />
             </Grid>
@@ -74,6 +99,7 @@ export default function Login() {
                 id="lastName"
                 label="Last Name"
                 name="lastName"
+                ref={lNameRef}
                 autoComplete="lname"
               />
             </Grid>
@@ -85,6 +111,7 @@ export default function Login() {
                 id="email"
                 label="Email Address"
                 name="email"
+                ref={emailRef}
                 autoComplete="email"
               />
             </Grid>
@@ -97,12 +124,26 @@ export default function Login() {
                 label="Password"
                 type="password"
                 id="password"
+                ref={passwordRef}
                 autoComplete="current-password"
               />
             </Grid>
-            
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="PasswordConfirmation"
+                label="Password Confirm"
+                type="password"
+                id="passwordConfirmation"
+                ref={passwordConfirmRef}
+                autoComplete="current-passwordConfirmation"
+              />
+            </Grid>
           </Grid>
           <Button
+            disable={loading}
             type="submit"
             fullWidth
             variant="contained"
@@ -120,8 +161,6 @@ export default function Login() {
           </Grid>
         </form>
       </div>
-      
     </Container>
-    
   );
 }
